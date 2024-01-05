44468dfb2884e5fc13685822984c88387e35fe7a
Felipe Baltor <fbaltor@gmail.com>
2024-01-04 23:31:39 -0300

    clean: removed unused code

--- format_tools.py
+++
@@ -1,8 +0,0 @@
-
-with open('tools.txt', 'r+') as f:
-    tools = ['"' + line.rstrip()[1:-3] + '"' for line in f]
-    f.seek(0)
-    for tool in tools:
-        f.write(f"{tool},\n")
-    f.truncate()
-
